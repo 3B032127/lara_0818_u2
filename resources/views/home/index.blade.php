1e4f9edbb03742bc944a51211101989e2473f266
rwei920821 <72637277+rwei920821@users.noreply.github.com>
2023-10-24 12:07:45 +0800

    create master layouts

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>test web</title>
+</head>
+<body>
+    @extends('layouts.master')
+
+    @section('title', 'home')
+
+    @section('content')
+    <h1>Home</h1>
+    <p>This is my body content.</p>
+    @endsection
+</body>
+</html>
